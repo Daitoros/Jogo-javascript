eacc6e1f759c28d6b12a9918f191213d272a8d76
Luis Henrique <daviabmedeiros@gmail.com>
2024-03-08 17:03:50 -0300

    step 20

---
+++ script.js
@@ -0,0 +1,13 @@
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeapon = 0;
+let fighting;
+let monsterHealth;
+// string
+// let inventory = "stick"; 
+//Array de string
+// let inventory = ["stick", "dagger", "sword"];
+let inventory = ["stick"];
+//Para dar match em um elemento css/html com uma variável js
+let button1 = document.querySelector("#button1")
