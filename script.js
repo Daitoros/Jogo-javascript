c119f920951e54091f3ec95b47d4a11d5882f24c
Luis Henrique <daviabmedeiros@gmail.com>
2024-03-09 16:22:08 -0300

    step 54

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ let gold = 50;
 let currentWeapon = 0;
 let fighting;
 let monsterHealth;
-// string
-// let inventory = "stick"; 
-//Array de string
-// let inventory = ["stick", "dagger", "sword"];
+/* String
+        let inventory = "stick"; 
+    Array de string
+        let inventory = ["stick", "dagger", "sword"];*/
 let inventory = ["stick"];
-//Para dar match em um elemento css/html com uma variável js
-//let button1 = document.querySelector("#button1")
+/*  Para dar match em um elemento css/html com uma variável js
+    let button1 = document.querySelector("#button1")*/
 
-//Valor não variável
+//Valor não variável (Const)
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
@@ -24,3 +24,57 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth"); //Changed name because of ive already declared a variable named monsterHealth up above
+const locations = [{ //An object IN an array
+    name: "town square" //key/pairs. name of the property and it values
+}]; 
+/*  Initialize buttons
+    One way to access properties in javascript is by dot notation
+    When this button is clicked, this function will be activated*/
+    button1.onclick = goStore;
+    button2.onclick = goCave;
+    button3.onclick = fightDragon;
+
+function update(location) {
+
+}
+
+function goTown() {
+  button1.innerText = "Comprar 10 de vida (10 ouros)" // It will changes the innerText of the button
+  button2.innerText = "Comprar arma (30 ouro)";
+  button3.innerText = "Ir para o Centro da Cidade";
+
+  // Updating buttons functions
+  button1.onclick = goStore;
+  button2.onclick = goCave;
+  button3.onclick = fightDragon;
+
+  //uptadint the main text
+  text.innerText = "Você está no centro da cidade. Você vê uma placa que diz \"Loja\"."
+}
+// Declarando funções
+function goStore() {
+  button1.innerText = "Comprar 10 de vida (10 ouros)" // It will changes the innerText of the button
+  button2.innerText = "Comprar arma (30 ouro)";
+  button3.innerText = "Ir para o Centro da Cidade";
+
+  // Updating buttons functions
+  button1.onclick = buyHealth;
+  button2.onclick = buyWeapon;
+  button3.onclick = goTown;
+
+  //uptadint the main text
+  text.innerText = "Você entra na loja."
+}
+
+  function goCave() {
+    console.log("Indo para a caverna.");
+  }
+  function fightDragon() {
+    console.log("Lutando com dragão.");
+  }
+  function buyHealth() {
+
+  }
+  function buyWeapon() {
+  
+  }
