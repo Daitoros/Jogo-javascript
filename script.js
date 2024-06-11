c6135da9fedd8ba861982908384a243b8ffa26dc
Daitoros <daviabmedeiros@gmail.com>
2024-06-11 18:46:51 -0300

    step  158

--- script.js
+++ script.js
@@ -211,8 +211,9 @@ function goStore() {
         defeatMonster();
       }
     }
-    if(Math.random() <= .1){
+    if(Math.random() <= .1 && inventory.length !== 1){
       text.innerText += " Your " + inventory.pop() + " breaks."; //making the possibility of your weapon to break
+      currentWeaponIndex--;
     }
   }
 
